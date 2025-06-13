29661d1eb2774e437df5ef2e0762151d3a713699
Arun <arunsk1310@gmail.com>
2025-06-13 19:57:28 +0530

    page setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1A202C',
+        accent: '#00bfa6',
+      },
+    },
+  },
+  plugins: [],
+}
